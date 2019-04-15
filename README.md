63807ed1f33912eb780a3d3bac3a9746bfbe0fb9
Karol Rozwałka (MacBook Pro) <karol.rozwalka@webevo.pl>
2019-04-15 14:02:56 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+## Instalation
+
+* Open in Google Chrome browser url: chrome://extensions/
+* Click the button "Load unpacked extension"
+* Select folder with extension from your disk
---
+++ content.js
@@ -0,0 +1,29 @@
+var host = window.location.hostname;
+var skipTime = 60;
+
+host = host.replace('www.', '');
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "clicked_browser_action" ) {
+
+            // console.log(host);
+
+            if(host == 'player.pl') {
+                /* Skip ads on player.pl */
+                jQuery('.ima-ad-container').find('video')[0].currentTime = skipTime;
+                console.log('Skipped! ;-)');
+            }
+
+            if(host == 'vod.tvp.pl') {
+                document.getElementById('JS-TVPlayer').contentWindow.document.getElementById('tvplayer').contentWindow.document.getElementsByTagName('video')[0].currentTime = skipTime;
+                console.log('Skipped! ;-)');
+            }
+
+            if(host == 'ipla.tv') {
+                jQuery('.cpp2-ads-container').find('video')[0].currentTime = skipTime;
+                console.log('Skipped! ;-)');
+            }
+        }
+    }
+);
